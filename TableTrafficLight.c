4153ba5d81d44b83b9a7c69d65c9b454480529be
Max Wang <maxwang051@gmail.com>
2016-03-05 17:41:08 -0600

    wrote FSM

--- TableTrafficLight.c
+++ TableTrafficLight.c
@@ -31,13 +31,54 @@
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 
+#define GoS 0
+#define GoSW 1
+#define GoSP 2
+#define WaitSW 3
+#define WaitSP 4
+#define WaitSWP 5
+#define GoW 6
+#define GoWP 7
+#define WaitWS 8
+#define WaitWP 9
+#define WaitWSP 10
+#define GoP 11
+#define WaitPS 12
+#define WaitPW 13
+#define WaitPSW 14
+
+
+typedef const struct State {
+	uint32_t trafficOut;
+	uint32_t pedestrianOut;
+	uint32_t waitTime;
+	uint32_t nextState[8];
+} State;
+
+State FSM[18] = {
+	{0x21, 0x03, 3000, {GoS, WaitSW, GoS, WaitSW, WaitSP, WaitSWP, WaitSP, WaitSWP}}, // GoS
+	{0x21, 0x03, 3000, {WaitSW, WaitSW, WaitSW, WaitSW, WaitSW, WaitSW, WaitSW, WaitSW}}, // GoSW
+	{0x21, 0x03, 3000, {WaitSP, WaitSP, WaitSP, WaitSP, WaitSP, WaitSP, WaitSP, WaitSP}}, // GoSP
+	{0x22, 0x03, 1000, {GoW, GoW, GoW, GoW, GoW, GoW, GoW, GoW}}, // WaitSW
+	{0x22, 0x03, 1000, {GoP, GoP, GoP, GoP, GoP, GoP, GoP, GoP}}, // WaitSP
+	{0x22, 0x03, 1000, {GoWP, GoWP, GoWP, GoWP, GoWP, GoWP, GoWP, GoWP}}, // WaitSWP
+	{0x0C, 0x03, 3000, {GoW, GoW, WaitWS, WaitWS, WaitWP, WaitWP, WaitWSP, WaitWSP}}, // GoW
+	{0x0C, 0x03, 3000, {WaitWP, WaitWP, WaitWP, WaitWP, WaitWP, WaitWP, WaitWP, WaitWP}}, // GoWP
+	{0x14, 0x03, 1000, {GoS, GoS, GoS, GoS, GoS, GoS, GoS, GoS}}, // WaitWS
+	{0x14, 0x03, 1000, {GoW, GoW, GoW, GoW, GoW, GoW, GoW, GoW}}, // WaitWP
+	{0x14, 0x03, 1000, {GoSP, GoSP, GoSP, GoSP, GoSP, GoSP, GoSP, GoSP}}, // WaitWSP
+	{0x24, 0x01, 3000, {GoP, WaitPW, WaitPS, WaitPSW, GoP, WaitPW, WaitPS, WaitPSW}}, // GoP
+	{0x24, 0x02, 1000, {GoS, GoS, GoS, GoS, GoS, GoS, GoS, GoS}}, // WaitPS
+	{0x24, 0x02, 1000, {GoW, GoW, GoW, GoW, GoW, GoW, GoW, GoW}}, // WaitPW
+	{0x24, 0x02, 1000, {GoSW, GoSW, GoSW, GoSW, GoSW, GoSW, GoSW, GoSW}} // WaitPSW
+};
 // ***** 3. Subroutines Section *****
 
 int main(void){ 
   TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210); // activate grader and set system clock to 80 MHz
 	
 	SYSCTL_RCGCGPIO_R |= 0x32;
-	int delay = SYSCTL_RCGCGPIO_R;
+	int delay = SYSCTL_RCGC2_R;
 	GPIO_PORTB_DIR_R |= 0x3F; 				// initialize port b
 	GPIO_PORTB_AFSEL_R &= 0xC0;
 	GPIO_PORTB_DEN_R |= 0x3F;
